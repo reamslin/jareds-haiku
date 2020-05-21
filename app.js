0f1c9939f3d12cbe760df8d3dcc0f01fa5a66ada
reamslin <reamslin@gmail.com>
2020-05-21 09:00:04 -0400

    original commit

---
+++ app.js
@@ -0,0 +1,36 @@
+// our 'database' of words
+
+const oneNouns = 
+const twoAdjectives =
+const fourAdjectives =
+const twoNouns = 
+const fiveAdjectives = 
+const threeAdjectives =
+
+
+/**
+ * Haiku structure:
+ * 4a 1n
+ * 2a 3a 2n
+ * 5a
+ */
+function generateHaiku() {
+    // need to get random word for each type of word
+
+    // then need to return a string of html formatting the haiku
+}
+
+function getRandomWord(words) {
+    randomIndex = Math.floor(Math.random() * words.length)
+    // get the word from the random index
+}
+
+// need to access the button and 'haiku' elements from the DOM
+const btn = 
+const haiku =
+
+// add an event listener for when a user clicks the button
+btn.addEventListener("click", function () {
+    // what should happen when a user clicks the button?
+    
+})
